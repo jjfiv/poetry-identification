a8b1e6610941394096c6bc90d00cbbd7cb9087a5
John Foley <jjfoley@smith.edu>
2020-01-07 15:30:16 -0500

    comment the training file

--- train_random_forest.py
+++ train_random_forest.py
@@ -10,14 +10,15 @@ from sklearn.metrics import roc_auc_score, precision_score, recall_score
 from sklearn.tree import _tree
 from collections import defaultdict
 
-
+# If it doesn't have the POETRY label, it's not.
+# Need numeric labels for learning.
 def label_to_y(label):
     if label == "POETRY":
         return 1
     else:
         return 0
 
-
+# Recursively flatten nested lists
 def flatten(xs):
     if type(xs) == list:
         for x in xs:
@@ -26,11 +27,12 @@ def flatten(xs):
     else:
         yield xs
 
-
+# recursively flatten, sort, and convert to a numpy array.
+# Used later to get the pages for each book into a big list of pages.
 def flat_arr(xs):
     return np.array(sorted(flatten(xs)))
 
-
+# Load the truth data from the JSONL file into these parallel lists:
 data = []
 ys = []
 books = []
@@ -44,53 +46,58 @@ with open("truth-data/truth.jsonl") as fp:
         books.append(instance["book"])
         pages.append(instance["page"])
 
+# assign numbers to teach feature
 fnums = DictVectorizer()
 xs = fnums.fit_transform(data)
 fnames = fnums.get_feature_names()
 
+# Get the truth data into sliceable numpy array
 ys = np.array(ys)
 
+# Group pages by book:
 by_book = defaultdict(list)
 for i in range(len(data)):
     by_book[books[i]].append(i)
 
+# construct a list of books:
 by_book = dict((book, nums) for book, nums in by_book.items())
 books = np.array(sorted(set(books)))
 
+# collect models from each of the folds:
 models = []
 measures = defaultdict(list)
 
+# split by book for robust training:
 folds = KFold(n_splits=10, shuffle=True, random_state=42)
 for train_b, test_b in folds.split(books):
     train_books = books[train_b]
     test_books = books[test_b]
 
+    # get the page ids for each book:
     train_i = flat_arr([by_book[b] for b in train_books])
     test_i = flat_arr([by_book[b] for b in test_books])
 
+    # train model on pages
     model = ExtraTreesClassifier(
         n_estimators=30, random_state=13, class_weight="balanced"
     )
     model.fit(xs[train_i], ys[train_i])
+    # hold onto it
     models.append(model)
+
+    # evaluate as we go
     yp = model.predict_proba(xs[test_i])[:, 1]
     AUC = roc_auc_score(ys[test_i], yp)
     measures["AUC"].append(AUC)
-
     print("AUC: %1.3f" % AUC)
-    for t in range(100):
-        tf = t / 100.0
-        prec = precision_score(ys[test_i], yp > tf)
-        recall = recall_score(ys[test_i], yp > tf)
-        if prec >= 0.8:
-            print(" %02d. P: %1.3f . R: %1.3f" % (t, prec, recall))
-            break
 
 
 def dump_tree(tree_model):
+    """Recursively turn a SKLearn Tree model into a python dictionary (which can be saved as JSON)"""
     tree = tree_model.tree_
 
     def recurse(node):
+        """Recursively handle a given node."""
         if tree.feature[node] != _tree.TREE_UNDEFINED:
             fid = int(tree.feature[node])
             threshold = float(tree.threshold[node])
@@ -105,11 +112,13 @@ def dump_tree(tree_model):
 
     return recurse(0)
 
-
+# Saving the feature names allows us to tell if this model is out of date
 forest = {
     "feature_names": fnames,
     "forest": [[dump_tree(e) for e in m.estimators_] for m in models],
 }
+
+# Actually save the data here:
 with open("forest.json", "w") as fp:
     json.dump(forest, fp)
 
